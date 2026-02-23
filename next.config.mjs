9f8404d796fdec9538e47b929c56b8e90a7a50a3
hot321ps-jpg <hot321ps@gmail.com>
2026-02-23 17:41:59 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
-/** @type {import('next').Next} */
-const nextConfig = {
-  reactStrictMode: true,
-};
-
-export default nextConfig;
+].map((tab) => {
+  const TabIcon = tab.icon;
+  return (
+    <button ...>
+      <TabIcon size={14} /> {tab.label}
+    </button>
+  );
+})}
